9ea87f1d10f3648b547b7aa3b704f8c1b3fdeefc
Hannes Muehleisen <hannes@muehleisen.org>
2016-04-29 13:58:26 +0200

    moving

--- clients/R/Tests/deps-install.R
+++
@@ -1,8 +0,0 @@
-# create lib folder if it should be missing to avoid the prompt
-dir.create(unlist(strsplit(Sys.getenv("R_LIBS_USER"), .Platform$path.sep))[1L], recursive = TRUE, showWarnings=F)
-
-# autoinstall DBI and digest, we need those to install MonetDB.R
-dd <- capture.output(suppressMessages(suppressWarnings({
-	install.packages(c("devtools", "digest"), repos="http://cran.rstudio.com/", quiet=T)
-	devtools::install_github("rstats-db/DBI", quiet=T)
-})))
--- clients/Tests/exports.py
+++
@@ -1,2 +0,0 @@
-import MonetDBtesting.listexports
-MonetDBtesting.listexports.main()
