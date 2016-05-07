35ce282fd187ac2b51b01b36a0923ada2fba6113
Hannes Muehleisen <hannes@muehleisen.org>
2016-05-07 12:32:21 +0200

    autoconf macro rename

--- buildandtest.sh
+++ buildandtest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export MAKEFLAGS="-j"
+rm MonetDBLite_*.tar.gz
 R CMD BUILD . && \
 R CMD INSTALL MonetDBLite_*.tar.gz && \
 (cd tests; R -f testthat.R)
--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 (cd src
 ./bootstrap
-# TODO: fix this
-# mv "buildtools/conf/lt~obsolete.m4" buildtools/conf/lt-obsolete.m4
-# find . -name "Makefile.in" -exec sed -e "s/lt~obsolete\.m4/lt-obsolete\.m4/g" {} \; -i
+# buildtools/conf/lt~obsolete.m4 file name violates CRAN policies, rename and replace references
+mv "buildtools/conf/lt~obsolete.m4" buildtools/conf/lt-obsolete.m4
+find . -type f -name "Makefile.in" -o -name aclocal.m4 -exec sed -i "" -e "s/lt~obsolete\.m4/lt-obsolete\.m4/g" {} \;
 ./configure
 (
 cd sql/server
-bison -y  -d -p sql sql_parser.y
+bison -y -d -p sql sql_parser.y
 mv y.tab.h sql_parser.tab.h
 mv y.tab.c sql_parser.tab.c
 )
