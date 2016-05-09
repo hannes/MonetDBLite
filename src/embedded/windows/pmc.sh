5993282af1b75990881b02af390d12343a9ffa7b
Hannes Muehleisen <hannes@muehleisen.org>
2016-05-09 09:58:12 +0200

    windows

--- src/embedded/windows/pmc.sh
+++ src/embedded/windows/pmc.sh
@@ -3,7 +3,7 @@ if [ $# -eq 1 ]; then
 	echo $1
 	# sedscript.in created using 
 	# grep -o "@[[:alnum:]_]*@" Makefile.in | sort -f | uniq | sed -e "s/^/s|/" -e "s/$/||/" > sedscript.tpl
-	sed -f src/tools/embedded/windows/sedscript $1 > $NN
+	sed -f src/embedded/windows/sedscript $1 > $NN
 else
 	touch src/config.status
 	find src -name "*.in" -type f -exec sh $0 {} \;
