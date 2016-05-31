7501e9ece7d9eb189bfe227af3bf094f724432a8
Hannes Muehleisen <hannes@muehleisen.org>
2016-05-31 13:55:44 +0200

    Fix #48, some threads were never started

--- src/mal/mal/mal_client.c
+++ src/mal/mal/mal_client.c
@@ -437,6 +437,7 @@ MCstopClients(Client cntxt)
 		if (cntxt != c){
 			if (c->mode == RUNCLIENT){
 				c->mode = FINISHCLIENT;
+				// TODO: have a function pointer in the client context for this call
 				SQLexitClient(c);
 			}
 			else if (c->mode == FREECLIENT)
