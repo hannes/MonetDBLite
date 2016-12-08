7bc852d83a481425dfab05c94bc57a074637e930
Mark Raasveldt <mark.raasveldt@gmail.com>
2016-12-08 16:18:11 +0100

    Added code for disconnecting a client.

--- src/embedded/embeddedpy.h
+++ src/embedded/embeddedpy.h
@@ -23,9 +23,11 @@ pyapi_export PyObject* python_monetdb_sql(PyObject* client, char* query);
 //! Inserts values into a SQL table, monetdb_init must be called first and "args" must be either (table_name, dictionary) or (table_name, list(column_names), list(values))
 pyapi_export PyObject* python_monetdb_insert(PyObject* client, char* schema, char* table_name, PyObject* values);
 //! Creates a new MonetDB client context and returns it as a PyClientObject
-pyapi_export PyObject *python_monetdb_client(void);
+pyapi_export PyObject* python_monetdb_client(void);
+//! Disconnect a MonetDB client context
+pyapi_export PyObject* python_monetdb_disconnect(PyObject* client);
 //! Shuts down the database in-process so python_monetdb_init can be called again
-pyapi_export PyObject *python_monetdb_shutdown(void);
+pyapi_export PyObject* python_monetdb_shutdown(void);
 
 
 pyapi_export void python_monetdblite_init(void);
