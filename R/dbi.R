f6879bf2f949be13b060d081162cc7e037afa19e
Hannes Muehleisen <hannes@muehleisen.org>
2017-01-04 11:27:47 +0100

    Package version in dbGetInfo

--- R/dbi.R
+++ R/dbi.R
@@ -159,7 +159,7 @@ setMethod("dbGetInfo", "MonetDBConnection", def=function(dbObj, ...) {
   ll <- as.list(envdata$value)
   names(ll) <- envdata$name
   ll$name <- "MonetDBConnection"
-  ll$db.version <- NA
+  ll$db.version <- packageVersion("MonetDBLite")
   ll$dbname <- ll$gdk_dbname
   ll$username <- NA
   ll$host <- NA
