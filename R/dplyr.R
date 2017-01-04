a702b48bf57c0dd099e668bbd31f21672e4399d0
Hannes Muehleisen <hannes@muehleisen.org>
2017-01-04 10:12:35 +0100

    rolling own src_acquire no longer required

--- R/dplyr.R
+++ R/dplyr.R
@@ -16,8 +16,6 @@ src_monetdb <- function(dbname="demo", host = "localhost", port = 50000L, user =
   dplyrMt[["sql_translate_env.MonetDBConnection"]]     <- sql_translate_env.MonetDBConnection
   dplyrMt[["src_desc.src_monetdb"]]                    <- src_desc.src_monetdb
   dplyrMt[["tbl.src_monetdb"]]                         <- tbl.src_monetdb
-  dplyrMt[["con_acquire.src_monetdb"]]                 <- con_acquire.src_monetdb
-  dplyrMt[["con_release.src_monetdb"]]                 <- con_release.src_monetdb
   dplyrMt[["db_query_fields.MonetDBConnection"]]       <- db_query_fields.MonetDBEmbeddedConnection
   dplyrMt[["db_query_rows.MonetDBConnection"]]         <- db_query_rows.MonetDBConnection
   dplyrMt[["db_query_rows.MonetDBEmbeddedConnection"]] <- db_query_rows.MonetDBEmbeddedConnection
@@ -60,13 +58,6 @@ src_desc.src_monetdb <- function(x) {
   } else {
     paste0("MonetDB ",x$info$monet_version, " (",x$info$monet_release, ")")
   }
-}
-
-con_acquire.src_monetdb <- function(src) {
-  src$obj
-}
-
-con_release.src_monetdb <- function(src, con) {
 }
 
 sample_n.tbl_monetdb <- function(x, size, replace = FALSE, weight = NULL) {
