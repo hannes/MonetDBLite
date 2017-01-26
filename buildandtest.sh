817140a091b65892409b4e6d4109a864b13a02d2
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2017-01-26 17:57:48 +0100

    Removed all existing stuff to avoid colisions

--- buildandtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export MAKEFLAGS="-j"
-export MONETDBLITE_DEBUG=42
-rm MonetDBLite_*
-R CMD BUILD . && \
-R CMD INSTALL --build MonetDBLite_*.tar.gz && \
-R CMD INSTALL MonetDBLite_*.tgz && \
-(cd tests; R -f testthat.R)
