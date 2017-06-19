ac7239019337fe8736a662d55c3c132b2bace238
Anthony Damico <ajdamico@gmail.com>
2017-06-19 17:13:05 -0400

    undeprecate external connection functions

--- R/control.R
+++ R/control.R
@@ -1,6 +1,5 @@
 monetdb.server.start <-
   function( bat.file ){
-    .Deprecated("Consider using MonetDBLite")
     if ( .Platform$OS.type == "unix" ) {
       if( !file.exists( bat.file ) ) stop( paste( bat.file , "does not exist. Run monetdb.server.setup() to create a batch file." ) )
       
@@ -69,7 +68,6 @@ monetdb.server.start <-
 
 # oh the humanity
 monetdb.server.stop <- function(correct.pid, wait=TRUE){
-  .Deprecated("monetdb.server.shutdown")
   if (length(correct.pid) != 1) {
     stop("Need single pid, got ", correct.pid)
   }
@@ -113,7 +111,6 @@ monetdb.server.setup <-
     # cannot be accessed at the same time
     dbport = 50000
   ){
-   .Deprecated("Consider using MonetDBLite")
     # switch all slashes to match windows
     monetdb.program.path <- normalizePath(getOption("monetdb.programpath.override", monetdb.program.path), mustWork = FALSE)
     # remove trailing slash from paths, otherwise the server won't start
@@ -238,7 +235,6 @@ monetdb.server.getpid <- function (conn) {
 
 # this is somewhat evil, no admin rights required to kill server. Even works on closed connections.
 monetdb.server.shutdown <- function(conn) {
-  .Deprecated("Consider using MonetDBLite")
   stopifnot(inherits(conn, "MonetDBConnection"))
   # reconnect with MAL scenario
   newparms <- conn@connenv$params
