8c14252f68f87e9675b3eb7ba398afeeb79ad06d
Hannes Muehleisen <hannes@muehleisen.org>
2017-06-23 14:58:22 +0200

    cleanup

--- src/embedded/defines.c
+++ src/embedded/defines.c
@@ -1,2 +1,8 @@
+// Poor Man's configure
 #include <stdio.h> 
-int main() {printf("-DSIZEOF_INT=%zu -DSIZEOF_LONG=%zu -DSIZEOF_LONG_LONG=%zu -DSIZEOF_SIZE_T=%zu -DSIZEOF_VOID_P=%zu\n", sizeof(int), sizeof(long), sizeof(long long), sizeof(size_t), sizeof(void*)); return 0;}
+
+int main() {
+	printf("-DSIZEOF_INT=%zu -DSIZEOF_LONG=%zu -DSIZEOF_LONG_LONG=%zu -DSIZEOF_SIZE_T=%zu -DSIZEOF_VOID_P=%zu\n",
+			sizeof(int), sizeof(long), sizeof(long long), sizeof(size_t), sizeof(void*));
+	return 0;
+}
