719ec36fb7500ec0d73ea51830e7d93f28a093f3
Hannes Muehleisen <hannes@muehleisen.org>
2017-07-10 12:54:20 +0200

    GDKusec be gone

--- buildandtest.sh
+++ buildandtest.sh
@@ -2,7 +2,7 @@
 export MAKEFLAGS="-j"
 export MONETDBLITE_DEBUG=42
 rm MonetDBLite_*
-R CMD BUILD . && \
+R CMD build . && \
 R CMD INSTALL --build MonetDBLite_*.tar.gz && \
 R CMD INSTALL MonetDBLite_*.tgz && \
 (cd tests; R -f testthat.R)
