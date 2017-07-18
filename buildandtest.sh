8be2d740e7884b68205d79c657485e85ba81f4f9
Hannes Muehleisen <hannes@muehleisen.org>
2017-07-18 13:14:33 +0200

    linker flags

--- buildandtest.sh
+++ buildandtest.sh
@@ -2,7 +2,6 @@
 export MAKEFLAGS="-j"
 export MONETDBLITE_DEBUG=42
 rm MonetDBLite_*
-make -C src -f Makevars init
 R CMD build . && \
 R CMD INSTALL --build MonetDBLite_*.tar.gz && \
 R CMD INSTALL MonetDBLite_*.tgz && \
