d4b3ae0729a088717a280ceead36d87850814f1e
Hannes Muehleisen <hannes@muehleisen.org>
2017-07-22 21:08:29 +0200

    minor fix

--- R/zzz.R
+++ R/zzz.R
@@ -2,12 +2,13 @@
 	tryCatch({
     	# this is a (dirty) hack so we don't need to depend on dplyr
   		dplyrMt <- getNamespace("dplyr")$.__S3MethodsTable__.
-		dplyrMt[["sql_translate_env.MonetDBConnection"]] <- dplyr_sql_translate_env
+		  dplyrMt[["sql_translate_env.MonetDBConnection"]] <- dplyr_sql_translate_env
   		dplyrMt[["db_analyze.MonetDBConnection"]] <- dplyr_db_analyze
   		dplyrMt[["db_explain.MonetDBLiteConnection"]] <- dplyr_db_analyze
   		
   		dplyrMt[["sample_n.tbl_monetdb"]] <- dplyr_sample_n
   		dplyrMt[["sample_frac.tbl_monetdb"]] <- dplyr_sample_frac
   		dplyrMt[["tbl.src_monetdb"]] <- dplyr_tbl_monetdb
-	})
+	}, error = function(){NA})
+  TRUE
 }
